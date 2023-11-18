75493fb3bd92ac31d6ddcbe3167a553280f4017c
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-11-18 16:21:11 +0400

    done

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-let price = document.querySelectorAll('#price')
-let cir = document.querySelectorAll('.circle')
+let price = document.querySelectorAll('#price');
+let cir = document.querySelectorAll('.circle');
 
-for (let i = 0;i < cir.length;i++){
-    cir[i].addEventListener('click', () =>{
-        cir[i].classList.toggle('rotate') 
-    })
+let price2 = 10;
+
+for (let i = 0; i < cir.length; i++) {
+    cir[i].addEventListener('click', () => {
+        cir[i].classList.toggle('rotate');
+      
+        for (let j = 0; j < price.length; j++) {
+           let currentPrice = Number(price[j].innerHTML);
+           
+           if (price2 <= currentPrice) {
+               currentPrice *= 10;
+           }
+           else {
+               currentPrice /= 10;
+           }
+           price[j].innerHTML = currentPrice.toFixed(1);
+        }
+    });
 }
