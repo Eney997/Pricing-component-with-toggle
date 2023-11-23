85a054001b67d3e2a2776802a9d0b0d2c278d745
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-11-23 15:23:01 +0400

    done

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-let price = document.querySelectorAll('#price');
-let cir = document.querySelectorAll('.circle');
+let priceElements = document.querySelectorAll('#price');
+let circleButton = document.querySelector('.circle');
+let isAnnually = true;
 
-let price2 = 10;
+circleButton.addEventListener('click', () => {
+    circleButton.classList.toggle('rotate');
 
-for (let i = 0; i < cir.length; i++) {
-    cir[i].addEventListener('click', () => {
-        cir[i].classList.toggle('rotate');
-      
-        for (let j = 0; j < price.length; j++) {
-           let currentPrice = Number(price[j].innerHTML);
-           
-           if (price2 = currentPrice) {
-               currentPrice *= 10;
-           }
-           else {
-               currentPrice /= 10;
-           }
-           price[j].innerHTML = currentPrice.toFixed(1);
+    priceElements.forEach(priceElement => {
+        let currentPrice = Number(priceElement.innerHTML);
+        if (isAnnually) {
+            currentPrice *= 10;
+        } else {
+            currentPrice /= 10; 
         }
+        priceElement.innerHTML = currentPrice.toFixed(2);
     });
-}
+
+    isAnnually = !isAnnually; 
+});
